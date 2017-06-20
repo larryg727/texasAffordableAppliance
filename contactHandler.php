142ba1c2e7041f7ec7d3f6826126f270b6320e6d
larryg727 <larrygonzales727@gmail.com>
2017-06-19 23:56:09 -0500

    -added php file to handle contact form emails and made contact form mobile responsive

---
+++ contactHandler.php
@@ -0,0 +1,38 @@
+<?php
+
+
+
+if(isset($_POST['submit'])) {
+    $name = test_input($_POST["name"]);
+    $email = test_input($_POST['email']);
+    $phone = test_input($_POST['phone']);
+    $message = test_input($_POST['message']);
+}
+
+$msg = $name . " has sent you a message.\n They can be reached at " . $email . " or " . $phone . "\n Their message is as follows: ";
+
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+
+$to = "txaffordable@gmail.com";
+$subject = "Message from visitor@ TexasAffordable.repair";
+$header = "From: " . $email;
+$msg = $msg . wordwrap($message,70);
+
+if(mail($to,$subject,$msg,$header)) {
+   echo "<script>
+alert('Message sent successfully.');
+window.location.href='index.html';
+</script>";
+//    header('Location: index.html');
+}else {
+     echo "<script>
+alert('Message failed to send. Please try again.');
+window.location.href='contact.html';
+</script>";
+}
+?>
